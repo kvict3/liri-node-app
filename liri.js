22073c849e000cf66981ae71d409d51b2890a5db
Kenny Victoria <kvict3@gmail.com>
2016-06-11 09:17:16 -0400

    final commit not completed

--- liri.js
+++ liri.js
@@ -4,15 +4,3 @@ var keys = require('./keys.js');
 // Gets all of twitterKeys bands from the keys file.
 var keyList = keys.twitterKeys;
 
-// Grabs the genre information
-if (process.argv[2]) {
-	var genre = process.argv[2];
-}
-
-for (var key in keyList) {
-
-	// If the genre matches the key then print that band.
-	if (key === genre || genre === undefined) {
-		console.log("A " + key + " band is " + keyList[key] + ".");
-	}
-}
